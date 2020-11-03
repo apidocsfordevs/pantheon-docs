10f166f6321818263119e2e66fcb4fddcb00b5cf
shalvah <diakon.ng@gmail.com>
2020-11-03 21:22:04 +0100

    Add Conversations to sidebar

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    "API Reference": ['doc1', 'doc2', 'doc3', 'payments'],
+    "API Reference": ['doc1', 'doc2', 'doc3', 'payments', 'conversations'],
     Features: ['mdx'],
   },
 };
